aec464f05f4843cc03b2aff9c8fd9a683a5d7fdd
Gabriel Salé <gabriel.sale88220@gmail.com>
2026-03-02 19:32:39 +0100

    Realisation des tests

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,12 @@ import { Hono } from 'hono'
 
 export const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.json({
+    success: true,
+    message: "World Cup Ticketing API"
+  });
+});
+
 
 export default app
